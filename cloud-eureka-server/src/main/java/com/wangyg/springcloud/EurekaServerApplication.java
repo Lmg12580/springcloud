1ccae329730be2c31419f96d92314ac1ec257edf
wangyg <1037327746@qq.com>
2020-12-01 18:51:32 +0800

    新增cloud-eureka-server工程，修改部分pom文件

---
+++ cloud-eureka-server/src/main/java/com/wangyg/springcloud/EurekaServerApplication.java
@@ -0,0 +1,23 @@
+package com.wangyg.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka启动类
+ *
+ * @author wangyg
+ * @version 1.0.0
+ * @description Eureka启动类
+ * @copyright Copyright ©️ 2020
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
