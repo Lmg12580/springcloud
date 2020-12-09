eb05d89b8422f43c84c4fb20ef647ed45923cc06
wangyg <1037327746@qq.com>
2020-12-09 12:20:11 +0800

    HystrixDashboard搭建

---
+++ cloud-consumer-hystrix-dashboard/src/main/java/com/wangyg/springcloud/HystrixDashboardApplication.java
@@ -0,0 +1,24 @@
+package com.wangyg.springcloud;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * hystrix图形化界面
+ *
+ * @author wangyg
+ * @version 1.0.0
+ * @description hystrix图形化界面
+ * @copyright Copyright ©️ 2020
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+}
